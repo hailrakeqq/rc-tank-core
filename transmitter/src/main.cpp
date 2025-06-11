9bcb09b287689651bd8cfe2aa8d9f5bbae4c93ff
hailrakeqq <hailrake@riseup.net>
2025-06-11 14:22:22 +0300

    Update main.cpp

--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -1,31 +1,84 @@
 #include <SPI.h>
 #include <RF24.h>
-#include <Arduino.h>
-RF24 radio(10, 9); // CE, CSN
 
+RF24 radio(10,8); // CE, CSN
+int motorPinA1 = 5;  
+int motorPinA2 = 6;  
+int motorPinB1 = 3;  
+int motorPinB2 = 9;  
+int currentLeftPWM = 0;
+int currentRightPWM = 0;
+int currentLeftDirection = 0;
+int currentRightDirection = 0;
 const byte address[6] = "00001";
 
 void setup()
 {
   Serial.begin(9600);
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_LOW);
-  radio.stopListening();
+  radio.startListening(); 
 
-  Serial.println("Transmitter ready");
+  pinMode(motorPinA1, OUTPUT);
+  pinMode(motorPinA2, OUTPUT);
+  pinMode(motorPinB1, OUTPUT);
+  pinMode(motorPinB2, OUTPUT);
+
+  Serial.println("Reciever ready");
 }
 
 void loop()
 {
-  int leftRaw = analogRead(A0);  // 0..1023
-  int rightRaw = analogRead(A1); // 0..1023
+  if (radio.available())
+  {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
 
-  int leftTrackSpeed = map(leftRaw, 0, 1023, -256, 256);   // -256..256
-  int rightTrackSpeed = map(rightRaw, 0, 1023, -256, 256); // -256..256
+    int left_track, right_track;
+    sscanf(text, "%d;%d", &left_track, &right_track);
 
-  char data[32];
-  sprintf(data, "%d;%d", leftTrackSpeed, rightTrackSpeed);
-  radio.write(&data, sizeof(data));
-  Serial.println("Data send: " + String(data));
+    Serial.print("left: ");
+    Serial.println(left_track);
+    Serial.print("right: ");
+    Serial.println(right_track);
+
+    // ====== Death zone ======
+    if (abs(left_track) < 10 && abs(right_track) < 10) {
+      analogWrite(motorPinA1, 0);
+      analogWrite(motorPinA2, 0);
+      analogWrite(motorPinB1, 0);
+      analogWrite(motorPinB2, 0);
+      return;
+    }
+
+    // ====== Left track ======
+    int leftPWM = constrain(abs(left_track), 0, 255);
+
+    if (left_track > 0) {
+      analogWrite(motorPinB1, 0);
+      analogWrite(motorPinB2, leftPWM); // forward
+    } else if (left_track < 0) {
+      analogWrite(motorPinB2, 0);
+      analogWrite(motorPinB1, leftPWM); // back
+    } else {
+      analogWrite(motorPinB1, 0);
+      analogWrite(motorPinB2, 0);
+    }
+
+    // ====== Right track ======
+    int rightPWM = constrain(abs(right_track), 0, 255);
+
+    if (right_track > 0) {
+      analogWrite(motorPinA1, 0);
+      analogWrite(motorPinA2, rightPWM); // forward
+    } else if (right_track < 0) {
+      analogWrite(motorPinA2, 0);
+      analogWrite(motorPinA1, rightPWM); // back
+    } else {
+      analogWrite(motorPinA1, 0);
+      analogWrite(motorPinA2, 0);
+    }
+  }
+  delay(10);
 }
